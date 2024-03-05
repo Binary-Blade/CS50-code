dd01181424b1fd01c78056a60b135b934fc4dfcd
Binary-Blade <codegrunt@proton.me>
2024-03-05 20:44:11 +0100

    * feat(no-vowels.c): Add program for transform vowels in numeric letters

---
+++ no-vowels.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+void replace(char let[]);
+
+int main(int argc, char *argv[]) {
+  if (argc > 1) {
+    replace(argv[1]);
+  }
+  return 0;
+}
+
+void replace(char let[]) {
+
+  int length = strlen(let);
+  for (int i = 0; i < length; i++) {
+    switch (let[i]) {
+    case 'a' & 'A':
+      printf("6");
+      break;
+
+    case 'e' & 'E':
+      printf("3");
+      break;
+
+    case 'o' & 'O':
+      printf("0");
+      break;
+    default:
+      printf("%c", let[i]);
+    }
+  }
+}
